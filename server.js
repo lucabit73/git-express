d47beb3d2f031800c2d3f05f6d6af525d2e9bdee
Luca Querci <lucabit73@gmail.com>
2014-12-03 11:46:29 +0100

    primo

---
+++ server.js
@@ -0,0 +1,43 @@
+
+var serialport = require("serialport");
+var SerialPort = serialport.SerialPort; // localize object constructor
+
+var config = require('./config.json');
+var serial = config.serial_port;
+ 
+var sp = new SerialPort(serial, {
+  parser: serialport.parsers.readline("\n")
+});
+
+sp.on("data", function (data) {
+    //quando arrivano dati da arduino...
+    //azioni differenti a seconda dello stato in cui mi trovo
+
+    data = data.substring(0, data.length-1);
+    console.log("Arduino say: ",data);
+
+    if (data == "Power ON - MASBOOTH"){
+        sp.write('Ready to Push!!');
+    }
+
+
+    if (data == "green"){
+    	console.log('green push');
+
+    	var spawn = require('child_process').spawn,
+		    //ls    = spawn('ls', ['-lh',''], { cwd: config.env });
+		    gitadd = spawn('git', ['push'], { cwd: config.env });
+
+		gitadd.stdout.on('data', function (data) {
+		  console.log('stdout: ' + data);
+		});
+
+		gitadd.stderr.on('data', function (data) {
+		  console.log('stderr: ' + data);
+		});
+
+		gitadd.on('close', function (code) {
+		  console.log('child process exited with code ' + code);
+		});
+    }      
+});
